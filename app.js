3e0de406acfd4af2159e74bdebcc2c779b5ba831
Kenzuke14 <80883814+Kenzuke14@users.noreply.github.com>
2022-01-31 12:58:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ const countdownfunction = setInterval(function () {
   const seconds = Math.floor(diff % (1000 * 60) / 1000);
 
   if (diff < 0) {
-    <iframe align="center" frameborder="no" height="400px" name="frame1" scrolling="auto" src="https://mintnow.netlify.app" style="border: 1px solid;" width="98%"></iframe>   
-  
-;
+    clockdiv.style.display = "none";
     clearInterval(countdownfunction);
   } else {
     clockdiv.querySelector(".days").innerHTML = days;
